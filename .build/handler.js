f51a2db998333d52aea3863119b84ece70957ae4
DJViau <djviau@gmail.com>
2018-04-27 00:10:20 -0400

    cryptogoods

---
+++ .build/handler.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var getKittyStickers_1 = require("./kitty/getKittyStickers");
+var invalidateKittyId_1 = require("./kitty/invalidateKittyId");
+module.exports.getKittyStickers = getKittyStickers_1.default;
+module.exports.invalidateKittyId = invalidateKittyId_1.default;
+//# sourceMappingURL=handler.js.map
